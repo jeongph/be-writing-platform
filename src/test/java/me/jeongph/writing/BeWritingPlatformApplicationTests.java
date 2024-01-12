1479894376bc4e4f22b5cf7629bd0696f037f76a
JeongUk Park <dnr1105@gmail.com>
2024-01-12 23:50:01 +0900

    init:

---
+++ src/test/java/me/jeongph/writing/BeWritingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jeongph.writing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeWritingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
